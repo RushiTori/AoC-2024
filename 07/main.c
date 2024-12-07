5df231f8a437b42dc5e723cbe07fad57feea3d05
RushiTori <Arcturusarc09@gmail.com>
2024-12-07 13:54:01 +0100

    completed day 7

--- 07/main.c
+++ 07/main.c
@@ -1,3 +1,5 @@
+#include <math.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -65,8 +67,111 @@ char** OpenInputAsStringArray() {
 	return res;
 }
 
+typedef struct Calibration {
+	uint64_t result;
+	uint64_t* operands;
+	uint32_t operandCount;
+	bool isValid;
+} Calibration;
+
+Calibration ParseCalibration(char* str) {
+	Calibration tmp = {0};
+
+	tmp.result = atoll(str);
+
+	for (uint32_t i = 0; str[i]; i++) {
+		if (str[i] == ' ') tmp.operandCount++;
+	}
+
+	tmp.operands = malloc(sizeof(uint64_t) * tmp.operandCount);
+	if (!tmp.operands) return (Calibration){0};
+
+	for (uint32_t i = 0, j = 0; str[i]; i++) {
+		if (str[i] == ' ') {
+			tmp.operands[j++] = atoll(str + i + 1);
+		}
+	}
+
+	return tmp;
+}
+
+bool CheckCaliInternal(uint64_t* values, uint64_t count, uint64_t current, uint64_t expected) {
+	if (!count) return current == expected;
+	return CheckCaliInternal(values + 1, count - 1, current * *values, expected) ||
+		   CheckCaliInternal(values + 1, count - 1, current + *values, expected);
+}
+
+void CheckCalibration(Calibration* calibration) {
+	calibration->isValid =
+		CheckCaliInternal(calibration->operands + 1, calibration->operandCount - 1,
+						  *calibration->operands, calibration->result);
+}
+
+uint64_t Concat64(uint64_t a, uint64_t b) { return a * pow(10, floor(log10(b)) + 1) + b; }
+
+bool CheckCaliConcatInternal(uint64_t* values, uint64_t count, uint64_t current,
+							 uint64_t expected) {
+	if (!count) return current == expected;
+	return CheckCaliConcatInternal(values + 1, count - 1, current * *values, expected) ||
+		   CheckCaliConcatInternal(values + 1, count - 1, current + *values, expected) ||
+		   CheckCaliConcatInternal(values + 1, count - 1, Concat64(current, *values), expected);
+}
+
+void CheckCalibrationConcat(Calibration* calibration) {
+	calibration->isValid =
+		CheckCaliConcatInternal(calibration->operands + 1, calibration->operandCount - 1,
+								*calibration->operands, calibration->result);
+}
+
 int32_t main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv) {
-	/* code */
+	char** input = OpenInputAsStringArray();
+	if (!input) exit(1);
+
+	uint32_t calibrationCount = 0;
+	while (input[calibrationCount]) calibrationCount++;
+
+	Calibration* calibrations = malloc(sizeof(Calibration) * calibrationCount);
+	if (!calibrations) {
+		for (uint32_t i = 0; i < calibrationCount; i++) {
+			free(input[i]);
+		}
+		free(input);
+		exit(1);
+	}
+
+	for (uint32_t i = 0; i < calibrationCount; i++) {
+		calibrations[i] = ParseCalibration(input[i]);
+		if (!calibrations[i].operands) {
+			while (i) free(calibrations[--i].operands);
+			free(calibrations);
+			while (i < calibrationCount) free(input[i++]);
+			free(input);
+			exit(1);
+		}
+		free(input[i]);
+	}
+	free(input);
+
+	uint64_t totalValidCalibration = 0;
+	uint64_t totalValidConcatCalibration = 0;
+
+	for (uint32_t i = 0; i < calibrationCount; i++) {
+		CheckCalibration(calibrations + i);
+		if (calibrations[i].isValid) {
+			totalValidCalibration += calibrations[i].result;
+		} else {
+			CheckCalibrationConcat(calibrations + i);
+			if (calibrations[i].isValid) {
+				totalValidConcatCalibration += calibrations[i].result;
+			}
+		}
+		free(calibrations[i].operands);
+	}
+	free(calibrations);
+
+	printf("Total correct calibration results : %lu\n", totalValidCalibration);
+	printf("Total correct calibration (with concat) results : %lu\n",
+		   totalValidCalibration + totalValidConcatCalibration);
 
 	return 0;
 }
