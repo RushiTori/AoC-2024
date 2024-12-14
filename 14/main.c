2d7b0c9d535caed899673ade04fc5a8013dc6f6c
RushiTori <Arcturusarc09@gmail.com>
2024-12-14 13:22:30 +0100

    completed day 14, I recommend searching for lots of consecutive # character, and also to runn at least 10 thousand times if you dare change the input

--- 14/main.c
+++ 14/main.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -65,8 +66,116 @@ char** OpenInputAsStringArray() {
 	return res;
 }
 
+typedef struct Robot {
+	int64_t px;
+	int64_t py;
+	int64_t vx;
+	int64_t vy;
+	int64_t lx;
+	int64_t ly;
+} Robot;
+
+Robot* ParseRobots(char** lines, uint32_t* robotCount) {
+	if (!lines || !robotCount) return NULL;
+	uint32_t count = 0;
+	while (lines[count]) count++;
+	if (!count) return NULL;
+	Robot* robots = malloc(sizeof(Robot) * count);
+	if (!robots) return NULL;
+	for (uint32_t i = 0; i < count; i++) {
+		robots[i].px = atoi(strchr(lines[i], '=') + 1);
+		robots[i].py = atoi(strchr(lines[i], ',') + 1);
+		robots[i].vx = atoi(strrchr(lines[i], '=') + 1);
+		robots[i].vy = atoi(strrchr(lines[i], ',') + 1);
+	}
+	*robotCount = count;
+	return robots;
+}
+
+#define WIDTH 101
+#define HEIGHT 103
+#define SECONDS 100
+
 int32_t main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv) {
-	/* code */
+	char** input = OpenInputAsStringArray();
+	if (!input) exit(1);
+	uint32_t robotCount = 0;
+	Robot* robots = ParseRobots(input, &robotCount);
+	for (uint32_t i = 0; input[i]; i++) {
+		free(input[i]);
+	}
+	free(input);
+	if (!robots) exit(1);
+
+	uint32_t nw = 0;
+	uint32_t ne = 0;
+	uint32_t sw = 0;
+	uint32_t se = 0;
+	for (uint32_t i = 0; i < robotCount; i++) {
+		robots[i].lx = robots[i].px + robots[i].vx * SECONDS;
+		while (robots[i].lx < 0) robots[i].lx += WIDTH;
+		robots[i].lx %= WIDTH;
+		robots[i].lx -= WIDTH / 2;
+
+		robots[i].ly = robots[i].py + robots[i].vy * SECONDS;
+		while (robots[i].ly < 0) robots[i].ly += HEIGHT;
+		robots[i].ly %= HEIGHT;
+		robots[i].ly -= HEIGHT / 2;
+
+		if (robots[i].ly < 0) {
+			if (robots[i].lx < 0) nw++;
+			if (robots[i].lx > 0) ne++;
+		}
+		if (robots[i].ly > 0) {
+			if (robots[i].lx < 0) sw++;
+			if (robots[i].lx > 0) se++;
+		}
+	}
+	uint64_t totalSafety = nw * ne * sw * se;
+
+	uint32_t seconds = 0;
+	printf("Enter the number of seconds you want the robots to run for : ");
+	scanf("%u", &seconds);
+	printf("Running the robots for %u seconds and printing results in img subfolder\n", seconds);
+
+	char board[WIDTH * HEIGHT] = {0};
+	char fileNameBuffer[64];
+
+	system("rm -rf img");
+	system("mkdir img");
+
+	FILE* output = fopen("img/robots.txt", "w");
+	if (!output) {
+		printf("%s : %s\n", fileNameBuffer, strerror(errno));
+		free(robots);
+		exit(1);
+	}
+
+	for (uint32_t t = 0; t < seconds; t++) {
+		fprintf(output, "%u:\n", t);
+
+		memset(board, ' ', WIDTH * HEIGHT);
+		for (uint32_t i = 0; i < robotCount; i++) {
+			board[robots[i].px + robots[i].py * WIDTH] = '#';
+			robots[i].px += robots[i].vx;
+			if (robots[i].px < 0) robots[i].px += WIDTH;
+			robots[i].px %= WIDTH;
+
+			robots[i].py += robots[i].vy;
+			if (robots[i].py < 0) robots[i].py += HEIGHT;
+			robots[i].py %= HEIGHT;
+		}
+		for (uint32_t i = 0; i < HEIGHT; i++) {
+			fprintf(output, "%.*s\n", WIDTH, board + i * WIDTH);
+		}
+
+		fprintf(output, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	}
+	fclose(output);
+
+	printf("The total safety after %d seconds is %lu\n", SECONDS, totalSafety);
+
+	free(robots);
 
 	return 0;
 }
